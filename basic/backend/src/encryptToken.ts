b6d694eb9a6f47888b4a2c9a9db67072b28ff243
Gabriel Luders <ludersdev@gmail.com>
2024-01-30 21:21:17 -0300

    trying with basic setup

---
+++ basic/backend/src/encryptToken.ts
@@ -0,0 +1,77 @@
+import crypto from 'crypto'
+
+export const CIPHER = 'AES-256-CBC'
+export const KEY = 'MySuperSecretKeyForParamsToken12'
+
+/* 
+
+const tokenObject = {
+    connection: {
+        type: "rdp",
+        settings: {
+            "hostname": "10.0.0.12",
+            "username": "Administrator",
+            "password": "pAsSwOrD",
+            "enable-drive": true,
+            "create-drive-path": true,
+            "security": "any",
+            "ignore-cert": true,
+            "enable-wallpaper": false
+        }
+    }
+};
+
+*/
+
+export interface ConnectionSettings {
+  hostname: string
+  port: string
+  'enable-drive': boolean
+  'create-drive-path': boolean
+  security: string
+  'ignore-cert': boolean
+  'enable-wallpaper': boolean
+  'disable-auth': boolean
+}
+
+export interface TokenObject {
+  connection: {
+    type: string
+    settings: ConnectionSettings
+  }
+}
+
+export function encryptToken(
+  settings: ConnectionSettings
+): string {
+  const tokenObject: TokenObject = {
+    connection: {
+      type: 'rdp',
+      settings,
+    },
+  }
+
+  const iv = crypto.randomBytes(16)
+  const cipher = crypto.createCipheriv(
+    CIPHER,
+    Buffer.from(KEY),
+    iv
+  )
+
+  let encrypted = cipher.update(
+    JSON.stringify(tokenObject),
+    'utf-8',
+    'base64'
+  )
+  encrypted += cipher.final('base64')
+
+  const data = {
+    iv: iv.toString('base64'),
+    value: encrypted,
+  }
+
+  const jsonStr = JSON.stringify(data)
+  const encoded = new Buffer(jsonStr).toString('base64')
+
+  return encoded
+}
---
+++ basic/backend/src/server.ts
@@ -0,0 +1,66 @@
+import GuacamoleLite from 'guacamole-lite'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import {
+  CIPHER,
+  ConnectionSettings,
+  KEY,
+  encryptToken,
+} from './encryptToken'
+
+const app = express()
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const guacdOptions = {
+  port: 4822,
+}
+
+const clientOptions = {
+  crypt: {
+    cypher: CIPHER,
+    key: KEY,
+  },
+  log: {
+    level: 'DEBUG',
+  },
+}
+
+const serverOptions = {
+  server,
+  path: '/guac',
+}
+
+new GuacamoleLite(
+  serverOptions,
+  guacdOptions,
+  clientOptions
+)
+
+app.get('/getToken', (req, res) => {
+  const settings: ConnectionSettings = {
+    hostname: '10.1.1.202',
+    port: '3392',
+    'create-drive-path': true,
+    'disable-auth': true,
+    'enable-drive': true,
+    'enable-wallpaper': true,
+    'ignore-cert': true,
+    security: 'any',
+  }
+
+  const token = encryptToken(settings)
+
+  res.send(token)
+})
+
+app.get('/', (req, res) => {
+  res.send({
+    hello: 'world',
+  })
+})
+
+server.listen(3000)
