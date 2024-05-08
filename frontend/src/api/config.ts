c73c48158776088eadc7e96b28eed3135a17f462
Gabriel Luders <ludersdev@gmail.com>
2024-05-08 10:25:52 -0300

    feat: vite env

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,4 +1,3 @@
-export const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? process.env.BACKEND_URI
-    : 'http://localhost:3000'
+export const baseUrl = import.meta.env.PROD
+  ? import.meta.env.VITE_BACKEND_URI
+  : 'http://localhost:3000'
